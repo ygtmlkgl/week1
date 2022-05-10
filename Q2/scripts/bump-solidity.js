6e8f116090768dc5c4c41da7c2981454812cbbf8
ygtmlkgl <103690697+ygtmlkgl@users.noreply.github.com>
2022-05-10 16:23:10 +0300

    Update bump-solidity.js

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -10,3 +10,9 @@ bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+
+let myContent = fs.readFileSync("./contracts/Multiplier3Verifier.sol",{encoding:'utf-8'});
+let myBumped = myContent.replace(solidityRegex,"pragma solidity ^0.8.0");
+myBumped = myBumped.replace(verifierRegex,'contract Multiplier3Verifier');
+
+fs.writeFileSync("./contracts/Multiplier3Verifier.sol",myBumped);
